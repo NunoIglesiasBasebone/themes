127a7f30ce47f69e0cb6ebc9a48a99d7cb0b1c37
daniel-ramirez-basebone <daniel.ramirez@basebone.com>
2018-09-17 17:00:13 +0200

    THEMES::VE-PRODUCTPLATFORM * Delete double // from assets routes

--- src/Themes.php
+++ src/Themes.php
@@ -436,6 +436,9 @@ class Themes
             $themeAssetURL = $segments[0];
         }
 
+        //Remove unnecessary //
+        $themeAssetURL = str_replace('//', '/', $themeAssetURL);
+
         // If versioning is active, use the processed asset route as a parameter to elixir, which will return
         // the versioned file route from the assets manifest on /public/build/rev-manifest.json
         // If versioning is not active, encapsulate URL using url() in order to get the absolute URL instead
